a310b2bb139eb7c2d6c4450ecaf20579c4820aef
Ahmad Mairi <136258442+A-mairi@users.noreply.github.com>
2024-08-13 20:11:53 +0200

    Delete Day 01 directory

--- Day 01/Band-Name-Generator-Project.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the Band Name Generator.")
-
-city_name = input("Which city did you grow up in?\n")
-
-pet_name = input("Please insert a name of a pet?\n")
-
-print("Your band name could be: " + city_name + " " + pet_name)
